69ab32d672d623b38ac650e949d4863f82e7d73f
MD:Al-Fayej <mdal-fayej@MDAl-Fayejs-Mac-mini.local>
2025-09-09 23:57:04 +0600

    splash screen update

--- lib/Screen/splash/splash_screen.dart
+++ lib/Screen/splash/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:bdservice/Custom_Widget/custom_button.dart';
 import 'package:bdservice/helper/routes.dart';
+import 'package:bdservice/utils/style.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -8,18 +10,22 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
 
-        children: [
+          children: [
 
-          Text('Splash Screen Design'),
+            Text('Splash Screen Design',style: TextStyle(color: Colors.amber),),
+            SizedBox(height: 10),
+            CustomButton(onTap: (){
 
-          TextButton(onPressed: (){
-            Navigator.pushNamed(context, PageRoutNames.homeScreen);
-          }, child:Text('Route'))
-        ],
+              Navigator.pushNamed(context,PageRoutNames.homeScreen);
+            }, text: 'Route')
+          ],
+        ),
       ),
     );
 
---
+++ lib/helper/internet_exception.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+
+class AppException implements Exception{
+
+  final _message;
+   final _prifix;
+
+  AppException([this._message,this._prifix]);
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '$_message,$_prifix';
+
+  }
+
+}
+
+  class NoInternetException extends AppException{
+
+    NoInternetException([String? message] ) :super(message,'No Internet')  ;
+
+  }
